e581f6074ee59a7ae0a327f8addae7c8b9781242
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2023-08-02 15:50:44 +0330

    update next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
-const { withSentryConfig } = require('@sentry/nextjs');
-
-const moduleExports = {
- // next config
-};
-
-const sentryWebpackPluginOptions = {
- // sentry config
-}
-
-const withPlugins = require('next-compose-plugins')
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
- enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = async (phase, { defaultConfig }) => {
- delete defaultConfig['webpackDevMiddleware'];
-
- return withPlugins(
-     [withBundleAnalyzer],
-     withSentryConfig(moduleExports, sentryWebpackPluginOptions)
- )(phase, { defaultConfig });
-}
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+  }
+   
+  module.exports = nextConfig
