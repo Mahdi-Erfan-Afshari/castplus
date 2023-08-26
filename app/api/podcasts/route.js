c0b908190b0eb39930cd4e735904757bf082060c
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2023-08-27 00:10:45 +0330

    update

--- app/api/podcasts/route.js
+++ app/api/podcasts/route.js
@@ -3,7 +3,7 @@ import podcasts from "./server.json"
 
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://castplus.netlify.app'
+export const server = dev ? 'http://localhost:3000' : 'https://fluffy-sundae-2c87e7.netlify.app'
 
 export async function GET(request) {
     return NextResponse.json(podcasts)
