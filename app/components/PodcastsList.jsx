399ffccb0814f2e58e413f6dfca82a815d7feb5e
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2023-08-27 13:09:22 +0330

    update

--- app/components/PodcastsList.jsx
+++ app/components/PodcastsList.jsx
@@ -31,11 +31,11 @@ const PodcastPage = async () => {
 				  	<Image class="hidden sm:block w-32 h-32 sm:w-48 sm:h-auto object-cover border-e-[1px] border-gray" src={podcast.thumbnail} alt="" width="0" height="0" />
 					<div className='flex items-center'>
 				  		<Image class="w-20 h-20 sm:w-48 sm:h-auto sm:rounded-none object-cover rounded-lg block sm:hidden" src={podcast.thumbnail} alt="" width="0" height="0" />
-				  		<h1 className='text-md ms-4 block sm:hidden'>{podcast.title}</h1>
+				  		<h1 className='text-md font-semibold ms-4 block sm:hidden'>{podcast.title}</h1>
 					</div>
 				  <div class="sm:pt-6 pt-1 sm:p-5 text-center sm:text-left space-y-4">
 				    <blockquote>
-						<h1 className='sm:text-2xl lg:text-3xl hidden sm:block'>{podcast.title}</h1>
+						<h1 className='sm:text-2xl lg:text-3xl font-semibold hidden sm:block'>{podcast.title}</h1>
 				    	<p class="lg:text-lg sm:text-sm text-xs text-Gray font-medium mt-2 md:mt-4">{podcast.description}</p>
 				    </blockquote>
 				    <figcaption class="font-medium">
