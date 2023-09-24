0ab083239c1b75fb93ebb5bf6345dd5e22824ece
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2023-09-24 18:54:50 +0330

    update

--- app/lib/mongo.js
+++ app/lib/mongo.js
@@ -8,7 +8,7 @@ const uri = process.env.MONGODB_URI
 const options = {}
 
 let client
-let clientPromise: Promise<MongoClient>
+let clientPromise
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local')
