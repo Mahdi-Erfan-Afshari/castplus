d341c0e5b826699037e91098ea4763f2ced0a6e6
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2023-09-24 19:56:07 +0330

    update

--- app/api/editUser/route.js
+++ app/api/editUser/route.js
@@ -1,13 +1,6 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/app/lib/mongo";
 
-export async function GET(req) {
-	const client = await clientPromise
-	var db = client.db('castplus')
-	var doc = await db.collection('user_profile').find().toArray()
-	return NextResponse.json(doc)
-}
-
 export async function POST(req) {
 	const client = await clientPromise
 	var db = client.db('castplus')
