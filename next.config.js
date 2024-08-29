6ca2bbf6ebaa1aff4aaef82aea477e3d89790a4b
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2024-08-29 15:02:07 +0330

    add real episode

--- next.config.js
+++ next.config.js
@@ -4,33 +4,38 @@ const nextConfig = {
 		appDir: true,
 	},
 	images: {
-		domains: ['github.com', 'lh3.googleusercontent.com', 'deow9bq0xqvbj.cloudfront.net', 'cdn-arch.shenoto.com']
-		// remotePatterns: [
-		// 	{
-		// 		protocol: 'https',
-		// 		hostname: 'github.com',
-		// 		port: '',
-		// 		pathname: '/**',
-		// 	},
-		// 	{
-		// 		protocol: 'https',
-		// 		hostname: 'lh3.googleusercontent.com',
-		// 		port: '',
-		// 		pathname: '/**',
-		// 	},
-		// 	{
-		// 		protocol: 'https',
-		// 		hostname: 'deow9bq0xqvbj.cloudfront.net',
-		// 		port: '',
-		// 		pathname: '/**',
-		// 	},
-		// 	{
-		// 		protocol: 'https',
-		// 		hostname: 'cdn-arch.shenoto.com',
-		// 		port: '',
-		// 		pathname: '/**',
-		// 	},
-		// ],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'github.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'deow9bq0xqvbj.cloudfront.net',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn-arch.shenoto.com',
+				port: '',
+				pathname: '/shenoto-channel/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn-arch.shenoto.com',
+				port: '',
+				pathname: '/**',
+			},
+		],
 	}
 }
 
